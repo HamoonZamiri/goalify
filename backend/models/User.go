2ac9c02a299a529f66c9b0d54ea0dbe68caa4d0d
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-02 00:06:14 -0400

    feat: add user DTO

--- backend/models/User.go
+++ backend/models/User.go
@@ -16,3 +16,14 @@ type User struct {
 	Id                 uuid.UUID `db:"id" json:"id"`
 	RefreshToken       uuid.UUID `db:"refresh_token" json:"refresh_token"`
 }
+
+type UserDTO struct {
+	RefreshTokenExpiry time.Time `db:"refresh_token_expiry" json:"refresh_token_expiry"`
+	Email              string    `db:"email" json:"email"`
+	AccessToken        string    `json:"access_token"`
+	Xp                 int       `db:"xp" json:"xp"`
+	LevelId            int       `db:"level_id" json:"level_id"`
+	CashAvailable      int       `db:"cash_available" json:"cash_available"`
+	Id                 uuid.UUID `db:"id" json:"id"`
+	RefreshToken       uuid.UUID `db:"refresh_token" json:"refresh_token"`
+}
