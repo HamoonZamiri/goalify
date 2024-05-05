3a9d20311cd9187bd245c4f68d5bd962f7a0de79
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-04 23:43:57 -0400

    feat: signup and health check integration tests

--- backend/go.mod
+++ backend/go.mod
@@ -6,4 +6,12 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ backend/main_test.go
@@ -0,0 +1,71 @@
+package main_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"goalify"
+	"goalify/db"
+	"goalify/entities"
+	"goalify/users/handler"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const BASE_URL = "http://localhost:8080"
+
+func setup() {
+	go main.Run()
+	time.Sleep(2 * time.Second)
+}
+
+func TestHealth(t *testing.T) {
+	res, err := http.Get(BASE_URL + "/health")
+	assert.Nil(t, err)
+	assert.Equal(t, 200, res.StatusCode)
+}
+
+func TestSignup(t *testing.T) {
+	reqBody := handler.SignupRequest{Email: "user@mail.com", Password: "password"}
+	stringifiedBody, err := json.Marshal(reqBody)
+  assert.Nil(t, err)
+
+	res, err := http.Post(BASE_URL + "/api/users/signup", "application/json", bytes.NewReader(stringifiedBody))
+	assert.Nil(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	var user entities.UserDTO
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	assert.Nil(t, err)
+
+	json.Unmarshal(body, &user)
+	assert.Equal(t, "user@mail.com", user.Email)
+}
+
+func TestSignupEmailExists(t *testing.T) {
+  reqBody := handler.SignupRequest{Email: "user@mail.com", Password: "password"}
+  stringifiedBody, err := json.Marshal(reqBody)
+  assert.Nil(t, err)
+  
+  res, _ := http.Post(BASE_URL + "/api/users/signup", "application/json", bytes.NewReader(stringifiedBody))
+  assert.Equal(t, res.StatusCode, http.StatusBadRequest)
+}
+
+func TestMain(m *testing.M) {
+  db, err := db.New("goalify")
+  if err != nil {
+    panic(err)
+  }
+  setup()
+  code := m.Run()
+
+  query := `DELETE from users;`
+  db.MustExec(query)
+  os.Exit(code)
+}
