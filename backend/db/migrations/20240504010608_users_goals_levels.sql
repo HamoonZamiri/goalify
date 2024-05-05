0fee15af6bbb4aa34e6e1955fc79d8083d189bd4
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-04 23:40:01 -0400

    feat: create initial db migration

---
+++ backend/db/migrations/20240504010608_users_goals_levels.sql
@@ -0,0 +1,49 @@
+-- +goose Up
+CREATE TABLE levels  (
+    id SERIAL PRIMARY KEY,
+    level_up_xp INTEGER NOT NULL,
+    cash_reward INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE users  (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    xp INTEGER DEFAULT 0,
+    level_id SERIAL REFERENCES levels(id),
+    cash_available INTEGER DEFAULT 0,
+    refresh_token UUID DEFAULT gen_random_uuid(),
+    refresh_token_expiry TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE goal_categories  (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR NOT NULL,
+    xp_per_goal INTEGER NOT NULL,
+    user_id UUID REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TYPE goal_status AS ENUM ('complete', 'not_complete');
+
+CREATE TABLE goals (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR NOT NULL,
+    description VARCHAR DEFAULT '',
+    user_id UUID REFERENCES users(id),
+    category_id UUID REFERENCES goal_categories(id),
+    status goal_status DEFAULT 'not_complete',
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
+
+-- +goose Down
+DROP TABLE goals;
+DROP TABLE goal_categories;
+DROP TABLE users;
+DROP TABLE levels;
