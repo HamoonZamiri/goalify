13f12da33eab7b9d8ac14459d34979a92bb8c519
Porom <porom.kamal@gmail.com>
2024-05-06 20:55:57 -0400

    feat: Added frontend framework files for Vue.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="text-lg font-semibold">
+    Goalify
+  </div>
+  
+</template>
