60c360a8578899bf3e35db4ddd1359b02f4c6dc1
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-07 16:33:39 -0400

    fix: generalize scripts to use relative path instead of system

--- scripts/goose-reset.sh
+++ scripts/goose-reset.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-GOOSE_DRIVER=postgres GOOSE_DBSTRING="user=goalify password=goalify dbname=goalify host=localhost port=5432 sslmode=disable" goose -v -dir "/Users/hamoonzamiri/Desktop/Laptop/Projects/goalify/backend/db/migrations/" reset
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Set the relative path to the migrations directory
+MIGRATIONS_DIR="$SCRIPT_DIR/../backend/db/migrations/"
+GOOSE_DRIVER=postgres GOOSE_DBSTRING="user=goalify password=goalify dbname=goalify host=localhost port=5432 sslmode=disable" goose -v -dir "$MIGRATIONS_DIR" reset
