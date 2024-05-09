b621b135582dc069f577b2887fa88694100cba45
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-09 16:04:10 -0400

    feat: reorder params to (field update, ids) for consistency

--- backend/goals/service/GoalService.go
+++ backend/goals/service/GoalService.go
@@ -8,16 +8,16 @@ import (
 
 type GoalService interface {
 	// goals
-	CreateGoal(title, description string, userId, categoryId uuid.UUID) (entities.Goal, error)
-	UpdateGoalStatus(goalId uuid.UUID, status string) (entities.Goal, error)
-	GetGoalsByUserId(userId uuid.UUID) ([]entities.Goal, error)
-	UpdateGoalTitle(title string, goalId uuid.UUID) (entities.Goal, error)
-	UpdateGoalDescription(description string, goalId uuid.UUID) (entities.Goal, error)
-	GetGoalById(goalId uuid.UUID) (entities.Goal, error)
+	CreateGoal(title, description string, userId, categoryId uuid.UUID) (*entities.Goal, error)
+	UpdateGoalStatus(status string, goalId, userId uuid.UUID) (*entities.Goal, error)
+	GetGoalsByUserId(userId uuid.UUID) ([]*entities.Goal, error)
+	UpdateGoalTitle(title string, goalId, userId uuid.UUID) (*entities.Goal, error)
+	UpdateGoalDescription(description string, goalId, userId uuid.UUID) (*entities.Goal, error)
+	GetGoalById(goalId uuid.UUID) (*entities.Goal, error)
 
 	// categories
-	CreateGoalCategory(title string, xpPerGoal int, userId uuid.UUID) (entities.GoalCategory, error)
-	GetGoalCategoriesByUserId(userId uuid.UUID) ([]entities.GoalCategory, error)
-	UpdateGoalCategory(categoryId uuid.UUID, goalId uuid.UUID) (entities.GoalCategory, error)
-	GetGoalCategoryById(categoryId uuid.UUID) (entities.GoalCategory, error)
+	CreateGoalCategory(title string, xpPerGoal int, userId uuid.UUID) (*entities.GoalCategory, error)
+	GetGoalCategoriesByUserId(userId uuid.UUID) ([]*entities.GoalCategory, error)
+	UpdateGoalCategory(categoryId uuid.UUID, goalId, userId uuid.UUID) (*entities.GoalCategory, error)
+	GetGoalCategoryById(categoryId uuid.UUID) (*entities.GoalCategory, error)
 }
--- backend/goals/service/GoalServiceImpl.go
+++ backend/goals/service/GoalServiceImpl.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"goalify/entities"
@@ -34,124 +35,131 @@ func isEmptyField(field string) bool {
 	return field == ""
 }
 
-func (gs *GoalServiceImpl) CreateGoal(title, description string, userId, categoryId uuid.UUID) (entities.Goal, error) {
+func (gs *GoalServiceImpl) CreateGoal(title, description string, userId, categoryId uuid.UUID) (*entities.Goal, error) {
 	if isEmptyField(title) {
-		return entities.Goal{}, errors.New("title cannot be empty")
+		return nil, errors.New("title cannot be empty")
 	}
 	if isEmptyField(description) {
-		return entities.Goal{}, errors.New("description cannot be empty")
+		return nil, errors.New("description cannot be empty")
 	}
 
 	if isInvalidUUID(categoryId) {
-		return entities.Goal{}, errors.New("category id cannot be empty")
+		return nil, errors.New("category id cannot be empty")
 	}
 
 	if isInvalidUUID(userId) {
-		return entities.Goal{}, errors.New("invalid uuid")
+		return nil, errors.New("invalid uuid")
 	}
 
 	return gs.goalStore.CreateGoal(title, description, userId, categoryId)
 }
 
-func (gs *GoalServiceImpl) UpdateGoalStatus(userId, goalId uuid.UUID, status string) (entities.Goal, error) {
+func (gs *GoalServiceImpl) UpdateGoalStatus(status string, goalId, userId uuid.UUID) (*entities.Goal, error) {
 	cleanStatus := strings.ToLower(status)
 
 	if cleanStatus != "completed" && cleanStatus != "not_completed" {
-		return entities.Goal{}, errors.New("status must be either 'completed' or 'not_completed'")
+		return nil, errors.New("status must be either 'completed' or 'not_completed'")
 	}
 
 	goal, err := gs.goalStore.GetGoalById(goalId)
 	if err != nil {
-		return entities.Goal{}, err
+		return nil, err
 	}
 	if goal.UserId != userId {
-		return entities.Goal{}, errors.New("user does not own this goal")
+		return nil, errors.New("user does not own this goal")
 	}
 
 	return gs.goalStore.UpdateGoalStatus(goalId, status)
 }
 
-func (gs *GoalServiceImpl) GetGoalsByUserId(userId uuid.UUID) ([]entities.Goal, error) {
-	return gs.goalStore.GetGoalsByUserId(userId)
+func (gs *GoalServiceImpl) GetGoalsByUserId(userId uuid.UUID) ([]*entities.Goal, error) {
+	goals, err := gs.goalStore.GetGoalsByUserId(userId)
+	if err == sql.ErrNoRows {
+		return goals, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return goals, nil
 }
 
-func (gs *GoalServiceImpl) UpdateGoalTitle(title string, userId, goalId uuid.UUID) (entities.Goal, error) {
+func (gs *GoalServiceImpl) UpdateGoalTitle(title string, goalId, userId uuid.UUID) (*entities.Goal, error) {
 	if title == "" {
-		return entities.Goal{}, errors.New("title cannot be empty")
+		return nil, errors.New("title cannot be empty")
 	}
 	goal, err := gs.goalStore.GetGoalById(goalId)
 	if err != nil {
-		return entities.Goal{}, err
+		return nil, err
 	}
 	if goal.UserId != userId {
-		return entities.Goal{}, errors.New("user does not own this goal")
+		return nil, errors.New("user does not own this goal")
 	}
 
 	return gs.goalStore.UpdateGoalTitle(title, goalId)
 }
 
-func (gs *GoalServiceImpl) UpdateGoalDescription(description string, userId, goalId uuid.UUID) (entities.Goal, error) {
+func (gs *GoalServiceImpl) UpdateGoalDescription(description string, goalId, userId uuid.UUID) (*entities.Goal, error) {
 	if isEmptyField(description) {
-		return entities.Goal{}, errors.New("description cannot be empty")
+		return nil, errors.New("description cannot be empty")
 	}
 
 	goal, err := gs.goalStore.GetGoalById(goalId)
 	if err != nil {
-		return entities.Goal{}, err
+		return nil, err
 	}
 	if goal.UserId != userId {
-		return entities.Goal{}, errors.New("user does not own this goal")
+		return nil, errors.New("user does not own this goal")
 	}
 	return gs.goalStore.UpdateGoalDescription(description, goalId)
 }
 
-func (gs *GoalServiceImpl) GetGoalById(goalId uuid.UUID) (entities.Goal, error) {
+func (gs *GoalServiceImpl) GetGoalById(goalId uuid.UUID) (*entities.Goal, error) {
 	return gs.goalStore.GetGoalById(goalId)
 }
 
-func (gs *GoalServiceImpl) CreateGoalCategory(title string, xpPerGoal int, userId uuid.UUID) (entities.GoalCategory, error) {
+func (gs *GoalServiceImpl) CreateGoalCategory(title string, xpPerGoal int, userId uuid.UUID) (*entities.GoalCategory, error) {
 	if isEmptyField(title) {
-		return entities.GoalCategory{}, errors.New("title cannot be empty")
+		return nil, errors.New("title cannot be empty")
 	}
 	if xpPerGoal <= 0 || xpPerGoal > XP_PER_GOAL_MAX {
-		return entities.GoalCategory{}, fmt.Errorf("xp per goal must be between 1 and %d", XP_PER_GOAL_MAX)
+		return nil, fmt.Errorf("xp per goal must be between 1 and %d", XP_PER_GOAL_MAX)
 	}
 
 	if isInvalidUUID(userId) {
-		return entities.GoalCategory{}, errors.New("invalid uuid")
+		return nil, errors.New("invalid uuid")
 	}
 	return gs.goalCategoryStore.CreateGoalCategory(title, xpPerGoal, userId)
 }
 
-func (gs *GoalServiceImpl) GetGoalCategoriesByUserId(userId uuid.UUID) ([]entities.GoalCategory, error) {
+func (gs *GoalServiceImpl) GetGoalCategoriesByUserId(userId uuid.UUID) ([]*entities.GoalCategory, error) {
 	if isInvalidUUID(userId) {
-		return []entities.GoalCategory{}, errors.New("invalid uuid")
+		return []*entities.GoalCategory{}, errors.New("invalid uuid")
 	}
 	return gs.goalCategoryStore.GetGoalCategoriesByUserId(userId)
 }
 
-func (gs *GoalServiceImpl) UpdateGoalCategory(userId, categoryId, goalId uuid.UUID) (entities.GoalCategory, error) {
+func (gs *GoalServiceImpl) UpdateGoalCategory(categoryId, goalId, userId uuid.UUID) (*entities.GoalCategory, error) {
 	if isInvalidUUID(categoryId) {
-		return entities.GoalCategory{}, errors.New("invalid uuid category id")
+		return nil, errors.New("invalid uuid category id")
 	}
 	if isInvalidUUID(goalId) {
-		return entities.GoalCategory{}, errors.New("invalid uuid goal id")
+		return nil, errors.New("invalid uuid goal id")
 	}
 	gc, err := gs.goalCategoryStore.GetGoalCategoryById(categoryId)
 	if err != nil {
-		return entities.GoalCategory{}, err
+		return nil, err
 	}
 
 	if gc.UserId != userId {
-		return entities.GoalCategory{}, errors.New("user does not own this category")
+		return nil, errors.New("user does not own this category")
 	}
 
 	return gs.goalCategoryStore.UpdateGoalCategory(categoryId, goalId)
 }
 
-func (gs *GoalServiceImpl) GetGoalCategoryById(categoryId uuid.UUID) (entities.GoalCategory, error) {
+func (gs *GoalServiceImpl) GetGoalCategoryById(categoryId uuid.UUID) (*entities.GoalCategory, error) {
 	if isInvalidUUID(categoryId) {
-		return entities.GoalCategory{}, errors.New("invalid uuid category id")
+		return nil, errors.New("invalid uuid category id")
 	}
 	return gs.goalCategoryStore.GetGoalCategoryById(categoryId)
 }
