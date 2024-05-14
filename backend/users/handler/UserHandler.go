b5bbf93ac8a10755cec238b7ff964f72bcccf83e
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-14 15:06:37 -0400

    fix: reorder error handling logic to check validation errors first

--- backend/users/handler/UserHandler.go
+++ backend/users/handler/UserHandler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"errors"
 	"fmt"
 	"goalify/jsonutil"
 	"goalify/responses"
@@ -19,18 +18,14 @@ func NewUserHandler(userService service.UserService) *UserHandler {
 	return &UserHandler{userService}
 }
 
-func getErrorCode(err error) int {
-	if errors.Is(err, svcerror.ErrBadRequest) {
-		return http.StatusBadRequest
-	}
-	if errors.Is(err, svcerror.ErrNotFound) {
-		return http.StatusNotFound
-	}
-	return http.StatusInternalServerError
-}
-
 func (h *UserHandler) HandleSignup(w http.ResponseWriter, r *http.Request) {
 	decoded, problems, err := jsonutil.DecodeValid[SignupRequest](r)
+	if len(problems) > 0 {
+		apiErr := responses.NewAPIError("invalid request body", problems)
+		jsonutil.Encode(w, r, http.StatusBadRequest, apiErr)
+		return
+	}
+
 	if err != nil {
 		slog.Error("json decode: ", "err", err)
 		apiErr := responses.NewAPIError(err.Error(), nil)
@@ -38,15 +33,9 @@ func (h *UserHandler) HandleSignup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if len(problems) > 0 {
-		apiErr := responses.NewAPIError("invalid request body", problems)
-		jsonutil.Encode(w, r, http.StatusBadRequest, apiErr)
-		return
-	}
-
 	user, err := h.userService.SignUp(decoded.Email, decoded.Password)
 	if err != nil {
-		status := getErrorCode(err)
+		status := svcerror.GetErrorCode(err)
 		apiErr := responses.NewAPIError(err.Error(), nil)
 		jsonutil.Encode(w, r, status, apiErr)
 		return
@@ -61,15 +50,15 @@ func (h *UserHandler) HandleSignup(w http.ResponseWriter, r *http.Request) {
 
 func (h *UserHandler) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	decoded, problems, err := jsonutil.DecodeValid[LoginRequest](r)
-	if err != nil {
-		slog.Error("json decode: ", "err", err)
-		apiErr := responses.NewAPIError("error decoding request body", nil)
+	if len(problems) > 0 {
+		apiErr := responses.NewAPIError("invalid request body", problems)
 		jsonutil.Encode(w, r, http.StatusBadRequest, apiErr)
 		return
 	}
 
-	if len(problems) > 0 {
-		apiErr := responses.NewAPIError("invalid request body", problems)
+	if err != nil {
+		slog.Error("json decode: ", "err", err)
+		apiErr := responses.NewAPIError(err.Error(), nil)
 		jsonutil.Encode(w, r, http.StatusBadRequest, apiErr)
 		return
 	}
@@ -77,7 +66,7 @@ func (h *UserHandler) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	user, err := h.userService.Login(decoded.Email, decoded.Password)
 	if err != nil {
 		apiErr := responses.NewAPIError(err.Error(), nil)
-		jsonutil.Encode(w, r, getErrorCode(err), apiErr)
+		jsonutil.Encode(w, r, svcerror.GetErrorCode(err), apiErr)
 		return
 	}
 
@@ -92,18 +81,20 @@ func (h *UserHandler) HandleRefresh(w http.ResponseWriter, r *http.Request) {
 	decoded, err := jsonutil.Decode[RefreshRequest](r)
 	if err != nil {
 		apiErr := responses.NewAPIError(fmt.Errorf("error decoding request body: %w", err).Error(), nil)
-		jsonutil.Encode(w, r, getErrorCode(err), apiErr)
+		jsonutil.Encode(w, r, svcerror.GetErrorCode(err), apiErr)
 		return
 	}
 
 	user, err := h.userService.Refresh(decoded.UserId, decoded.RefreshToken)
 	if err != nil {
 		apiErr := responses.NewAPIError(err.Error(), nil)
-		jsonutil.Encode(w, r, getErrorCode(err), apiErr)
+		jsonutil.Encode(w, r, svcerror.GetErrorCode(err), apiErr)
 		return
 	}
 
-	if err := jsonutil.Encode(w, r, http.StatusOK, user); err != nil {
+	servResp := responses.New(user, "user refreshed")
+
+	if err := jsonutil.Encode(w, r, http.StatusOK, servResp); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		slog.Error("json encode: ", "err", err)
 		return
