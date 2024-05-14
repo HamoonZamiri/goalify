591517216eac2351e849bcea14924d34fcd24d06
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-14 15:06:14 -0400

    feat: move GetStatusCode func to error package

--- backend/svcerror/errors.go
+++ backend/svcerror/errors.go
@@ -1,9 +1,22 @@
 package svcerror
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+)
 
 var (
 	ErrBadRequest     = errors.New("bad request")
 	ErrInternalServer = errors.New("internal server error")
 	ErrNotFound       = errors.New("not found")
 )
+
+func GetErrorCode(err error) int {
+	if errors.Is(err, ErrBadRequest) {
+		return http.StatusBadRequest
+	}
+	if errors.Is(err, ErrNotFound) {
+		return http.StatusNotFound
+	}
+	return http.StatusInternalServerError
+}
