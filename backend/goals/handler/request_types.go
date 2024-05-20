7bfed4cf4e03da09f8965639222c482278dfff66
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-20 13:47:56 -0400

    fix: use constant in error message instead of hardcoded val

--- backend/goals/handler/request_types.go
+++ backend/goals/handler/request_types.go
@@ -1,6 +1,9 @@
 package handler
 
-import "goalify/options"
+import (
+	"fmt"
+	"goalify/options"
+)
 
 type CreateGoalRequest struct {
 	Title       string `json:"title"`
@@ -39,7 +42,7 @@ func (r CreateGoalCategoryRequest) Valid() map[string]string {
 	if r.XpPerGoal <= 0 {
 		problems["xp_per_goal"] = "xp_per_goal must be greater than 0"
 	} else if r.XpPerGoal > XP_MAX_PER_GOAL {
-		problems["xp_per_goal"] = "xp_per_goal must be less than 100"
+		problems["xp_per_goal"] = fmt.Sprintf("xp_per_goal must be less than %d", XP_MAX_PER_GOAL)
 	}
 	return problems
 }
@@ -52,6 +55,34 @@ func (r UpdateGoalCategoryRequest) Valid() map[string]string {
 
 	if r.XpPerGoal.IsPresent() && r.XpPerGoal.ValueOrZero() <= 0 {
 		problems["xp_per_goal"] = "xp_per_goal must be greater than 0"
+	}
+
+	if r.XpPerGoal.IsPresent() && r.XpPerGoal.ValueOrZero() > XP_MAX_PER_GOAL {
+		problems["xp_per_goal"] = fmt.Sprintf("xp_per_goal must be less than %d", XP_MAX_PER_GOAL)
+	}
+
+	return problems
+}
+
+func (r CreateGoalRequest) Valid() map[string]string {
+	problems := make(map[string]string)
+
+	if r.Title == "" {
+		problems["title"] = "title is required"
+	}
+	if len(r.Title) > TEXT_MAX_LEN {
+		problems["title"] = "title must be less than 255 characters"
+	}
+
+	if r.Description == "" {
+		problems["description"] = "description is required"
+	}
+	if len(r.Description) > TEXT_MAX_LEN {
+		problems["description"] = "description must be less than 255 characters"
+	}
+
+	if r.CategoryId == "" {
+		problems["category_id"] = "category_id is required"
 	}
 
 	return problems
