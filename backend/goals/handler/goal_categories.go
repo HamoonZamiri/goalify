0deeea2d2d4adb768683fc5f040c2914e7c314d6
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-20 20:18:26 -0400

    fix: refactor file names to follow snake case instead of camel case

--- backend/goals/handler/goal_categories.go
+++ backend/goals/handler/goal_categories.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"goalify/goals/service"
 	"goalify/jsonutil"
 	"goalify/middleware"
 	"goalify/responses"
@@ -11,62 +10,6 @@ import (
 
 	"github.com/google/uuid"
 )
-
-type GoalHandler struct {
-	goalService service.GoalService
-}
-
-func NewGoalHandler(goalService service.GoalService) *GoalHandler {
-	return &GoalHandler{goalService}
-}
-
-func (h *GoalHandler) HandleCreateGoal(w http.ResponseWriter, r *http.Request) {
-	body, problems, err := jsonutil.DecodeValid[CreateGoalRequest](r)
-	if len(problems) > 0 {
-		apiError := responses.NewAPIError("invalid request", problems)
-		jsonutil.Encode(w, r, http.StatusBadRequest, apiError)
-		return
-	}
-
-	if err != nil {
-		slog.Error("decode valid:", "err", err)
-		responses.SendAPIError(w, r, http.StatusInternalServerError, "error decoding request body", nil)
-		return
-	}
-
-	userId, err := middleware.GetIdFromHeader(r)
-	if err != nil {
-		slog.Error("middleware.GetIdFromHeader: ", "err", err)
-		responses.SendAPIError(w, r, http.StatusUnauthorized, err.Error(), nil)
-		return
-	}
-
-	parsedUserId, err := uuid.Parse(userId)
-	if err != nil {
-		slog.Error("parse userId: ", "err", err)
-		responses.SendAPIError(w, r, http.StatusInternalServerError, "error parsing user id", nil)
-		return
-	}
-
-	parsedCategoryId, err := uuid.Parse(body.CategoryId)
-	if err != nil {
-		slog.Error("parse categoryId: ", "err", err)
-		responses.SendAPIError(w, r, http.StatusInternalServerError, "error parsing category id", nil)
-		return
-	}
-
-	goal, err := h.goalService.CreateGoal(body.Title, body.Description, parsedUserId, parsedCategoryId)
-	if err != nil {
-		slog.Error("create goal: ", "err", err)
-		responses.SendAPIError(w, r, svcerror.GetErrorCode(err), err.Error(), nil)
-		return
-	}
-
-	if err := jsonutil.Encode(w, r, http.StatusOK, responses.New(goal, "goal created successfully")); err != nil {
-		slog.Error("json encode: ", "err", err)
-		responses.SendAPIError(w, r, http.StatusInternalServerError, "internal error encoding response", nil)
-	}
-}
 
 func (h *GoalHandler) HandleCreateGoalCategory(w http.ResponseWriter, r *http.Request) {
 	body, problems, err := jsonutil.DecodeValid[CreateGoalCategoryRequest](r)
--- backend/goals/handler/types.go
+++ backend/goals/handler/types.go
@@ -2,8 +2,19 @@ package handler
 
 import (
 	"fmt"
+	"goalify/goals/service"
 	"goalify/options"
+
+	"github.com/google/uuid"
 )
+
+type GoalHandler struct {
+	goalService service.GoalService
+}
+
+func NewGoalHandler(goalService service.GoalService) *GoalHandler {
+	return &GoalHandler{goalService}
+}
 
 type CreateGoalRequest struct {
 	Title       string `json:"title"`
@@ -21,6 +32,13 @@ type UpdateGoalCategoryRequest struct {
 	XpPerGoal options.Option[int]    `json:"xp_per_goal"`
 }
 
+type UpdateGoalRequest struct {
+	Title       options.Option[string] `json:"title"`
+	Description options.Option[string] `json:"description"`
+	CategoryId  options.Option[string] `json:"category_id"`
+	Status      options.Option[string] `json:"status"`
+}
+
 const (
 	TEXT_MAX_LEN    = 255
 	XP_MAX_PER_GOAL = 100
@@ -28,6 +46,11 @@ const (
 
 func NewGoalCategoryRequest(title string, xpPerGoal int) CreateGoalCategoryRequest {
 	return CreateGoalCategoryRequest{title, xpPerGoal}
+}
+
+func isValidUUID(id string) bool {
+	_, err := uuid.Parse(id)
+	return err == nil
 }
 
 func (r CreateGoalCategoryRequest) Valid() map[string]string {
@@ -85,5 +108,38 @@ func (r CreateGoalRequest) Valid() map[string]string {
 		problems["category_id"] = "category_id is required"
 	}
 
+	return problems
+}
+
+func (r UpdateGoalRequest) Valid() map[string]string {
+	problems := make(map[string]string)
+
+	if r.Title.IsPresent() && r.Title.ValueOrZero() == "" {
+		problems["title"] = "title cannot be empty"
+	}
+
+	if r.Title.IsPresent() && len(r.Title.ValueOrZero()) > TEXT_MAX_LEN {
+		problems["title"] = "title must be less than 255 characters"
+	}
+
+	if r.Description.IsPresent() && len(r.Description.ValueOrZero()) > TEXT_MAX_LEN {
+		problems["description"] = "description must be less than 255 characters"
+	}
+
+	if r.Description.IsPresent() && r.Description.ValueOrZero() == "" {
+		problems["description"] = "description cannot be empty"
+	}
+
+	if r.CategoryId.IsPresent() && r.CategoryId.ValueOrZero() == "" {
+		problems["category_id"] = "category_id cannot be empty"
+	}
+
+	if r.CategoryId.IsPresent() && !isValidUUID(r.CategoryId.ValueOrZero()) {
+		problems["category_id"] = "category_id must be a valid UUID"
+	}
+
+	if r.Status.IsPresent() && r.Status.ValueOrZero() != "completed" && r.Status.ValueOrZero() != "not_completed" {
+		problems["status"] = "status must be either 'completed' or 'not_completed'"
+	}
 	return problems
 }
