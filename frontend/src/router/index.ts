ae26de4ead61cc1064ed918e3ae8a4769f936d7f
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-05-24 19:23:06 -0400

    feat: add routes for login and register

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
+const routes = [
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
