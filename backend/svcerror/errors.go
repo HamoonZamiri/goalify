1ffca0831a9475177b392f2a0b5a3ad27cdf4655
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-02 17:18:27 -0400

    feat: add error for unauthorized resource access

--- backend/svcerror/errors.go
+++ backend/svcerror/errors.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	ErrBadRequest     = errors.New("bad request")
+	ErrUnauthorized   = errors.New("unauthorized")
 	ErrInternalServer = errors.New("internal server error")
 	ErrNotFound       = errors.New("not found")
 )
@@ -17,6 +18,9 @@ func GetErrorCode(err error) int {
 	}
 	if errors.Is(err, ErrNotFound) {
 		return http.StatusNotFound
+	}
+	if errors.Is(err, ErrUnauthorized) {
+		return http.StatusUnauthorized
 	}
 	return http.StatusInternalServerError
 }
