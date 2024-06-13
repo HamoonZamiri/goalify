3659d5b1bb342d1d0cb141cfb17109b2e255e30a
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-13 11:40:22 -0400

    feat: test middleware get by id function

---
+++ backend/middleware/middleware_test.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetIdFromHeader(t *testing.T) {
+	req, err := http.NewRequest("PUT", "localhost:8000/api", nil)
+	require.Nil(t, err)
+	req.Header.Set("user_id", "123")
+
+	id, err := GetIdFromHeader(req)
+	require.Nil(t, err)
+	assert.Equal(t, "123", id)
+}
