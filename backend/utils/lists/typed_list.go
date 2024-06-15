023819ce6adb63b5270d1155221ca959f109994b
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-15 16:13:52 -0400

    feat: add wrapper over builtin container/list that uses generics

---
+++ backend/utils/lists/typed_list.go
@@ -0,0 +1,74 @@
+package lists
+
+import (
+	"container/list"
+	"errors"
+)
+
+type TypedList[T comparable] struct {
+	list *list.List
+}
+
+func New[T comparable]() *TypedList[T] {
+	return &TypedList[T]{
+		list: list.New(),
+	}
+}
+
+func (tl *TypedList[T]) Len() int {
+	return tl.list.Len()
+}
+
+func (tl *TypedList[T]) PushBack(value T) {
+	tl.list.PushBack(value)
+}
+
+func (tl *TypedList[T]) PushFront(value T) {
+	tl.list.PushFront(value)
+}
+
+func (tl *TypedList[T]) Remove(value T) (T, error) {
+	if value == tl.list.Back().Value.(T) {
+		tl.list.Remove(tl.list.Back())
+		return value, nil
+	}
+
+	var assertedValue T
+	for e := tl.list.Front(); e != nil; e = e.Next() {
+		var ok bool
+		assertedValue, ok = e.Value.(T)
+		if !ok {
+			panic("lists: Remove(): type assertion failed")
+		}
+		if assertedValue == value {
+			tl.list.Remove(e)
+			return assertedValue, nil
+		}
+	}
+	return assertedValue, errors.New("lists: Remove(): value not found")
+}
+
+func (tl *TypedList[T]) Get(i int) (T, error) {
+	var assertedValue T
+	if i < 0 || i >= tl.list.Len() {
+		return assertedValue, errors.New("lists: Get(): index out of range")
+	}
+
+	if i == tl.list.Len()-1 {
+		return tl.list.Back().Value.(T), nil
+	}
+
+	e := tl.list.Front()
+	for j := 0; j < i; j++ {
+		e = e.Next()
+	}
+	assertedValue, ok := e.Value.(T)
+	if !ok {
+		panic("lists: Get(): type assertion failed")
+	}
+	return assertedValue, nil
+}
+
+func (tl *TypedList[T]) GetList() *list.List {
+	return tl.list
+}
---
+++ backend/utils/lists/typed_list_test.go
@@ -0,0 +1,84 @@
+package lists
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPushBack(t *testing.T) {
+	list := New[int]()
+	list.PushBack(1)
+	assert.Equal(t, 1, list.Len())
+}
+
+func TestPushFront(t *testing.T) {
+	list := New[int]()
+	list.PushFront(1)
+	assert.Equal(t, 1, list.Len())
+}
+
+func TestGet(t *testing.T) {
+	var value int
+	var err error
+
+	list := New[int]()
+	list.PushBack(1)
+	list.PushBack(2)
+	list.PushBack(3)
+
+	value, err = list.Get(0)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, value)
+
+	value, err = list.Get(1)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, value)
+
+	value, err = list.Get(2)
+	assert.Nil(t, err)
+	assert.Equal(t, 3, value)
+
+	_, err = list.Get(3)
+	assert.NotNil(t, err)
+
+	_, err = list.Get(-1)
+	assert.NotNil(t, err)
+}
+
+func TestRemove(t *testing.T) {
+	var value int
+	var err error
+
+	list := New[int]()
+	list.PushBack(1)
+	list.PushBack(2)
+	list.PushBack(3)
+
+	_, err = list.Remove(2)
+	assert.Nil(t, err)
+
+	value, err = list.Get(1)
+	assert.Nil(t, err)
+	assert.Equal(t, 3, value)
+}
+
+func TestRemoveBack(t *testing.T) {
+	var value int
+	var err error
+
+	list := New[int]()
+	list.PushBack(1)
+	list.PushBack(2)
+	list.PushBack(3)
+
+	_, err = list.Remove(3)
+	assert.Nil(t, err)
+
+	value, err = list.Get(1)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, value)
+
+	_, err = list.Get(2)
+	assert.NotNil(t, err)
+}
