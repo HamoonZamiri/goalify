a24a56b6e516b68201b59c835a3e899a59ae5f1f
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-20 14:46:27 -0400

    feat: add oh vue icons package for svg support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/index.css";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { IoCheckmarkCircleOutline } from "oh-vue-icons/icons";
+addIcons(IoCheckmarkCircleOutline);
 const app = createApp(App);
 
 app.use(router);
 
+app.component("v-icon", OhVueIcon);
 app.mount("#app");
