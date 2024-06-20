6e0b277c59ae8031e74d23dc01c5c9819181b862
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-20 16:36:07 -0400

    feat: add "/" route to router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-import GoalCategory from "./components/goals/GoalCategory.vue";
-import { ref } from "vue";
-import { type TGoalCategory, mockGoalCategory } from "./types";
-const categories = ref<TGoalCategory[]>([mockGoalCategory, mockGoalCategory]);
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <header class="bg-emerald-100 mb-2">
@@ -25,10 +20,5 @@ const categories = ref<TGoalCategory[]>([mockGoalCategory, mockGoalCategory]);
       </nav>
     </div>
   </header>
-  <section class="flex gap-4 px-6">
-    <div class="p-4 w-1/4" v-for="cat in categories">
-      <GoalCategory :goalCategory="cat" />
-    </div>
-  </section>
   <main><RouterView /></main>
 </template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../components/Login.vue";
 import Register from "../components/Register.vue";
+import Home from "@/components/Home.vue";
 const routes = [
   { path: "/login", component: Login },
   { path: "/register", component: Register },
+  { path: "/", component: Home },
 ];
 const router = createRouter({
   history: createWebHistory(),
