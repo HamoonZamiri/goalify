2649be979dfe13ae0714729dc4dd797f1139e1d2
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-20 16:36:19 -0400

    feat: create home page component

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import GoalCategory from "./goals/GoalCategory.vue";
+import { ref } from "vue";
+import { type TGoalCategory, mockGoalCategory } from "@/types";
+const categories = ref<TGoalCategory[]>([mockGoalCategory, mockGoalCategory]);
+</script>
+
+<template>
+  <section class="flex gap-4 px-6">
+    <div class="p-4 w-1/4" v-for="cat in categories">
+      <GoalCategory :goalCategory="cat" />
+    </div>
+  </section>
+</template>
