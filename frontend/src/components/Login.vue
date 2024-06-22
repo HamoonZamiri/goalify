22255c060dc047ebbc8dd8f9c221bbb842c59f4e
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-21 20:37:21 -0400

    feat: add login function to Login component

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const formData = ref<{ email: string; password: string }>({
+  email: "",
+  password: "",
+});
+
+const API_BASE = "http://localhost:3000/";
+async function login(payload: MouseEvent) {
+  payload.preventDefault();
+  console.log(formData.value);
+  const res = await fetch(`${API_BASE}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData.value),
+  });
+  if (res.ok) {
+    const json: unknown = await res.json();
+  }
+}
+</script>
 
 <template>
   <div class="w-full flex flex-col items-center">
@@ -9,6 +32,7 @@
       <div class="mb-4">
         <label class="">Email</label>
         <input
+          v-model="formData.email"
           class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-200 sm:text-sm sm:leading-6"
           type="email"
         />
@@ -16,11 +40,14 @@
       <div class="mb-6">
         <label>Password</label>
         <input
+          v-model="formData.password"
           class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="password"
         />
       </div>
-      <button class="w-full h-8 bg-blue-100 hover:bg-blue-200">Login</button>
+      <button @click="login" class="w-full h-8 bg-blue-100 hover:bg-blue-200">
+        Login
+      </button>
     </form>
   </div>
 </template>
