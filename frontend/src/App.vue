8d4ad1b7170baaec1dabcf30a6397d8bf654a32d
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-22 23:00:23 -0400

    feat: add logout functionality and conditional navbar based on auth status

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from "./router";
+import { isLoggedIn, removeUser } from "./utils/user";
+
+function handleLogout(e: MouseEvent) {
+  e.preventDefault();
+  removeUser();
+  router.push({ name: "Login" });
+}
+</script>
 
 <template>
   <header class="bg-emerald-100 mb-2">
@@ -7,16 +16,26 @@
         <h1 class="font-semibold text-3xl hover:text-blue-500">Goalify</h1>
       </RouterLink>
       <nav class="flex gap-4">
-        <RouterLink
-          class="font-semibold text-xl hover:text-blue-500"
-          to="/login"
-          >Login</RouterLink
-        >
-        <RouterLink
-          class="font-semibold text-xl hover:text-blue-500"
-          to="/register"
-          >Register</RouterLink
-        >
+        <div v-if="!isLoggedIn()" class="flex gap-2">
+          <RouterLink
+            class="font-semibold text-xl hover:text-blue-500"
+            to="/login"
+            >Login</RouterLink
+          >
+          <RouterLink
+            class="font-semibold text-xl hover:text-blue-500"
+            to="/register"
+            >Register</RouterLink
+          >
+        </div>
+        <div v-else>
+          <button
+            @click="handleLogout"
+            class="font-semibold text-xl hover:text-blue-500"
+          >
+            Log Out
+          </button>
+        </div>
       </nav>
     </div>
   </header>
