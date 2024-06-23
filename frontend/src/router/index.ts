13623cc3912325cd5e598f0f98e184d73a180921
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-06-22 22:12:03 -0400

    fix: use navigation guards to redirect to login page if user is not logged in

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,14 +2,21 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../components/Login.vue";
 import Register from "../components/Register.vue";
 import Home from "@/components/Home.vue";
+import { isLoggedIn } from "@/utils/user";
 const routes = [
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  { path: "/", component: Home },
+  { name: "Login", path: "/login", component: Login },
+  { name: "Register", path: "/register", component: Register },
+  { name: "Home", path: "/", component: Home },
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _) => {
+  if (!isLoggedIn() && to.name !== "Login" && to.name !== "Register") {
+    return { name: "Login" };
+  }
 });
 
 export default router;
