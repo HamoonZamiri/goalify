f4422dd71ca323559c313bca31f2dde7066cd815
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-07-19 19:41:35 -0400

    feat: dark modeify sign up and login components

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -43,33 +43,36 @@ async function signup(payload: MouseEvent) {
 
 <template>
   <div class="w-full flex flex-col items-center">
-    <h3 class="text-3xl font-semibold mb-6">Sign up for a new account</h3>
-    <form class="w-4/5 sm:w-2/5">
-      <div class="mb-4">
-        <label class="">Email</label>
+    <h3 class="text-3xl text-white mb-6">Sign up for a new account</h3>
+    <form class="w-4/5 sm:w-2/5 grid grid-cols-1 gap-4">
+      <div class="">
+        <label class="text-gray-200">Email</label>
         <input
           v-model="formData.email"
-          class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+          class="block h-10 w-full bg-gray-200 rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
           type="email"
         />
       </div>
-      <div class="mb-6">
-        <label>Password</label>
+      <div class="">
+        <label class="text-gray-200">Password</label>
         <input
           v-model="formData.password"
-          class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-50 sm:text-sm sm:leading-6"
+          class="block h-10 w-full bg-gray-200 rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
           type="password"
         />
       </div>
-      <div class="mb-6">
-        <label>Confirm Password</label>
+      <div class="mb-2">
+        <label class="text-gray-200">Confirm Password</label>
         <input
           v-model="formData.confirmPassword"
-          class="block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+          class="block h-10 w-full bg-gray-200 rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
           type="password"
         />
       </div>
-      <button @click="signup" class="w-full h-8 bg-blue-100 hover:bg-blue-200">
+      <button
+        @click="signup"
+        class="bg-blue-400 mt-4 rounded-lg text-gray-200 hover:bg-blue-500 h-10 py-1.5"
+      >
         Signup
       </button>
     </form>
