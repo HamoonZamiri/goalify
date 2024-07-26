81633fe2e15b1e424bd00cb0279c1232c03688e5
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-07-26 00:48:35 -0400

    fix: switch from className to class

--- frontend/src/components/ModalForm.vue
+++ frontend/src/components/ModalForm.vue
@@ -23,8 +23,7 @@ function setIsOpen(value: boolean) {
     />
   </div>
   <Dialog
-    className="absolute inset-0 h-screen flex justify-center items-center
-    hover:cursor-pointer bg-gray-600 z-10 w-screen bg-opacity-20 rounded-lg p-4"
+    class="absolute inset-0 h-screen flex justify-center items-center hover:cursor-pointer bg-gray-600 z-10 w-screen bg-opacity-20 rounded-lg p-4"
     :open="isOpen"
     @close="setIsOpen(false)"
   >
