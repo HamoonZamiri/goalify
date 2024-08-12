3294bf0167e4d422c678a80017871c5dd2f7a858
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-08-12 17:46:51 -0400

    refactor: change files to conform to idiomatic go standards

--- backend/goals/stores/goals.go
+++ backend/goals/stores/goals.go
@@ -9,6 +9,17 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+type GoalStore interface {
+	CreateGoal(title, description string, userId, categoryId uuid.UUID) (*entities.Goal, error)
+	UpdateGoalStatus(goalId uuid.UUID, status string) (*entities.Goal, error)
+	GetGoalsByUserId(userId uuid.UUID) ([]*entities.Goal, error)
+	UpdateGoalTitle(title string, goalId uuid.UUID) (*entities.Goal, error)
+	UpdateGoalDescription(description string, goalId uuid.UUID) (*entities.Goal, error)
+	GetGoalById(goalId uuid.UUID) (*entities.Goal, error)
+	UpdateGoalById(goalId uuid.UUID, updates map[string]interface{}) (*entities.Goal, error)
+	DeleteGoalById(goalId uuid.UUID) error
+}
+
 type GoalStoreImpl struct {
 	db *sqlx.DB
 }
