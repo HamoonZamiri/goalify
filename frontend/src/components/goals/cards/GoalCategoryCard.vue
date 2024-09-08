3b9ab1ecd500d83ccdb0fc7c75347550e7fe6a86
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-09-08 15:42:05 -0400

    feat: add information about user and level/curr xp

--- frontend/src/components/goals/cards/GoalCategoryCard.vue
+++ frontend/src/components/goals/cards/GoalCategoryCard.vue
@@ -113,19 +113,18 @@ watch(updates, async (category) => {
             </svg>
           </MenuButton>
           <MenuItems
-            class="absolute flex flex-col items-start w-56 bg-gray-300 p-1 rounded-md justify-self-start"
+            class="absolute flex flex-col items-start w-56 bg-gray-500 p-1 rounded-md justify-self-start right-0"
           >
             <MenuItem
               as="button"
-              class="w-full px-2 flex justify-start gap-x-2 hover:cursor-pointer hover:bg-gray-400 rounded-md bg-gray-300 text-gray-700"
+              class="w-full px-2 flex justify-start gap-x-2 bg-gray-600 hover:cursor-pointer hover:bg-gray-400 text-gray-700"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
-                stroke="currentColor"
-                class="size-5"
+                class="size-5 stroke-gray-300"
               >
                 <path
                   stroke-linecap="round"
@@ -133,33 +132,28 @@ watch(updates, async (category) => {
                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
                 />
               </svg>
-              <span>Edit</span>
+              <span class="text-sm text-gray-300">Edit</span>
             </MenuItem>
 
             <MenuItem
-              as="div"
-              class="w-full px-2 hover:cursor-pointer hover:bg-gray-400 rounded-md bg-gray-300 text-gray-700"
+              as="button"
+              class="w-full px-2 hover:cursor-pointer hover:bg-gray-400 bg-gray-600 text-gray-700 flex gap-x-2"
+              @click="handleDeleteCategory"
             >
-              <button
-                @click="handleDeleteCategory"
-                class="flex justify-start gap-x-2"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                class="size-5 stroke-gray-300"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="size-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  />
-                </svg>
-                <span>Delete</span>
-              </button>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                />
+              </svg>
+              <span class="text-sm text-gray-300">Delete</span>
             </MenuItem>
           </MenuItems>
         </Menu>
