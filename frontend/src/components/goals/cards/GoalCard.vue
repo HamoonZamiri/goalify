c65bf685e576b7c19d593b3f8773077ca74b8da0
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-09-10 18:30:54 -0400

    feat: create generic delete modal component

--- frontend/src/components/goals/cards/GoalCard.vue
+++ frontend/src/components/goals/cards/GoalCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Goal } from "@/utils/schemas";
-import { ref, h, watch, reactive } from "vue";
+import { ref, watch, reactive, nextTick } from "vue";
 import {
   Dialog,
   DialogPanel,
@@ -8,9 +8,11 @@ import {
   ListboxButton,
   ListboxOption,
   ListboxOptions,
+  TransitionRoot,
 } from "@headlessui/vue";
 import useGoals from "@/hooks/goals/useGoals";
 import useApi from "@/hooks/api/useApi";
+import DeleteModal from "@/components/DeleteModal.vue";
 const props = defineProps<{
   goal: Goal;
 }>();
@@ -37,6 +39,13 @@ function setIsEditing(value: boolean) {
 
 function setIsDeleting(value: boolean) {
   isDeleting.value = value;
+}
+
+function openEditingDialog() {
+  setIsEditing(false);
+  nextTick(() => {
+    setIsEditing(true);
+  });
 }
 
 async function handleDeleteGoal(e: MouseEvent) {
@@ -90,7 +99,7 @@ const statusMap = { not_complete: "Not Complete", complete: "Complete" };
 </script>
 <template>
   <header
-    @click="setIsEditing(true)"
+    @click="openEditingDialog()"
     class="flex p-4 w-full h-full bg-gray-700 hover:cursor-pointer hover:bg-gray-600 gap-x-2 items-center rounded-sm"
     :class="{
       'bg-green-600 hover:bg-green-700': props.goal.status === 'complete',
@@ -113,90 +122,71 @@ const statusMap = { not_complete: "Not Complete", complete: "Complete" };
     <span class="font-semibold text-gray-300">{{ props.goal.title }}</span>
   </header>
   <section>
-    <Dialog
-      class="absolute inset-0 h-screen flex justify-end hover:cursor-pointer z-10 w-screen bg-opacity-10 rounded-lg"
-      :open="isEditing"
-      @close="setIsEditing(false)"
+    <TransitionRoot
+      :show="isEditing"
+      appear
+      enter="transition-all ease-in-out duration-500 transform"
     >
-      <DialogPanel class="w-1/2">
-        <div
-          class="flex flex-col gap-4 h-full p-8 border-white bg-gray-800 hover:cursor-default shadow-md shadow-gray-400"
-        >
-          <input
-            v-model="updates.title"
-            class="w-full bg-gray-800 text-gray-200 focus:outline-none text-3xl"
-          />
-          <div class="flex gap-x-24 w-full text-gray-200">
-            <p class="text-xl">Status</p>
-            <div class="">
-              <Listbox v-model="updates.status">
-                <ListboxButton
-                  :class="{
-                    'w-56 h-8 text-center rounded-lg text-gray-600': true,
-                    'bg-green-400': updates.status === 'complete',
-                    'bg-orange-400': updates.status !== 'complete',
-                  }"
-                  >{{ statusMap[updates.status] }}</ListboxButton
-                >
-                <ListboxOptions class="mt-1">
-                  <ListboxOption
-                    v-for="status in statuses"
-                    :key="status.id"
-                    :value="status.value"
-                    :disabled="status.value === updates.status"
+      <Dialog
+        class="absolute inset-0 h-screen flex justify-end hover:cursor-pointer z-10 w-screen bg-opacity-10 rounded-lg"
+        @close="setIsEditing(false)"
+      >
+        <DialogPanel class="w-1/2">
+          <div
+            class="flex flex-col gap-4 h-full p-8 border-white bg-gray-800 hover:cursor-default shadow-md shadow-gray-400"
+          >
+            <input
+              v-model="updates.title"
+              class="w-full bg-gray-800 text-gray-200 focus:outline-none text-3xl"
+            />
+            <div class="flex gap-x-24 w-full text-gray-200">
+              <p class="text-xl">Status</p>
+              <div class="">
+                <Listbox v-model="updates.status">
+                  <ListboxButton
                     :class="{
-                      'w-56 hover:cursor-pointer h-8 bg-gray-300 text-gray-600 text-center hover:bg-gray-400': true,
-                      hidden: updates.status === status.value,
+                      'w-56 h-8 text-center rounded-lg text-gray-600': true,
+                      'bg-green-400': updates.status === 'complete',
+                      'bg-orange-400': updates.status !== 'complete',
                     }"
+                    >{{ statusMap[updates.status] }}</ListboxButton
                   >
-                    {{ status.name }}
-                  </ListboxOption>
-                </ListboxOptions>
-              </Listbox>
-            </div>
-          </div>
-          <textarea
-            v-model="updates.description"
-            class="w-full bg-gray-300 focus:outline-none h-64 p-2"
-          />
-          <button
-            class="w-full bg-red-400 hover:bg-red-500 text-gray-300 rounded-sm h-10"
-            @click="setIsDeleting(true)"
-          >
-            Delete This Goal
-          </button>
-
-          <Dialog
-            class="absolute inset-0 h-screen flex justify-center items-center hover:cursor-pointer z-20 w-screen bg-opacity-10 rounded-lg"
-            :open="isDeleting"
-            @close="setIsDeleting(false)"
-          >
-            <DialogPanel class="w-[25%] h-[25%]">
-              <div
-                class="border-2 border-white bg-gray-800 flex flex-col p-4 gap-y-4"
-              >
-                <p class="text-xl text-gray-300 text-center">
-                  Are you sure you want to delete this goal?
-                </p>
-                <div class="flex justify-center gap-4 text-gray-300">
-                  <button
-                    @click="setIsDeleting(false)"
-                    class="bg-gray-400 text-gray-700 rounded-lg p-2 hover:bg-gray-500"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    class="bg-red-400 text-gray-700 rounded-lg p-2 hover:bg-red-500"
-                    @click="handleDeleteGoal"
-                  >
-                    Yes, delete it
-                  </button>
-                </div>
+                  <ListboxOptions class="mt-1">
+                    <ListboxOption
+                      v-for="status in statuses"
+                      :key="status.id"
+                      :value="status.value"
+                      :disabled="status.value === updates.status"
+                      :class="{
+                        'w-56 hover:cursor-pointer h-8 bg-gray-300 text-gray-600 text-center hover:bg-gray-400': true,
+                        hidden: updates.status === status.value,
+                      }"
+                    >
+                      {{ status.name }}
+                    </ListboxOption>
+                  </ListboxOptions>
+                </Listbox>
               </div>
-            </DialogPanel>
-          </Dialog>
-        </div>
-      </DialogPanel>
-    </Dialog>
+            </div>
+            <textarea
+              v-model="updates.description"
+              class="w-full bg-gray-300 focus:outline-none h-64 p-2"
+            />
+            <button
+              class="w-full bg-red-400 hover:bg-red-500 text-gray-300 rounded-sm h-10"
+              @click="setIsDeleting(true)"
+            >
+              Delete This Goal
+            </button>
+            <DeleteModal
+              :is-open="isDeleting"
+              :set-opener="setIsDeleting"
+              delete-message="Are you sure you want to delete this goal?"
+              :delete-function="handleDeleteGoal"
+            />
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </TransitionRoot>
   </section>
 </template>
