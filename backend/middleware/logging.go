67251415ec7411cf8e02f0b4bfbc32cca9c225f5
Hamoon Zamiri <hamoonzamiri@Mac-mini.local>
2025-03-22 14:27:40 -0400

    feat(dev-ex): add Makefile command to pretty print json logs
    TIC-30

--- backend/middleware/logging.go
+++ backend/middleware/logging.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"time"
@@ -9,6 +12,11 @@ import (
 type statusCodeWriter struct {
 	w          http.ResponseWriter
 	statusCode int
+	body       *bytes.Buffer
+}
+
+var redactedKeys = map[string]bool{
+	"password": true,
 }
 
 func (scw *statusCodeWriter) WriteHeader(statusCode int) {
@@ -21,6 +29,7 @@ func (scw *statusCodeWriter) Header() http.Header {
 }
 
 func (scw *statusCodeWriter) Write(b []byte) (int, error) {
+	scw.body.Write(b)
 	return scw.w.Write(b)
 }
 
@@ -28,16 +37,40 @@ func (scw *statusCodeWriter) Flush() {
 	scw.w.(http.Flusher).Flush()
 }
 
+func prettifyAndRedactJSON(data []byte) interface{} {
+	var jsonData map[string]interface{}
+	defaultResponse := string(data)
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return defaultResponse
+	}
+
+	for key := range jsonData {
+		if redactedKeys[key] {
+			jsonData[key] = "[REDACTED]"
+		}
+	}
+	return jsonData
+}
+
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		statusWriter := &statusCodeWriter{w: w, statusCode: http.StatusOK}
+		statusWriter := &statusCodeWriter{w: w, statusCode: http.StatusOK, body: &bytes.Buffer{}}
 		next.ServeHTTP(statusWriter, r)
-		slog.Info("request",
-			slog.String("method", r.Method),
-			slog.String("path", r.URL.Path),
-			slog.Int("status", statusWriter.statusCode),
-			slog.Duration("duration", time.Since(start)),
+		prettyResponseBody := prettifyAndRedactJSON(statusWriter.body.Bytes())
+		durationLog := fmt.Sprintf("%dms", time.Since(start).Milliseconds())
+
+		slog.Info("incoming request",
+			slog.Group("request",
+				slog.String("method", r.Method),
+				slog.String("path", r.URL.Path),
+			),
+			slog.Group("response",
+				slog.Int("status", statusWriter.statusCode),
+				slog.String("duration", durationLog),
+				slog.Any("body", prettyResponseBody),
+				slog.Int("body_size", statusWriter.body.Len()),
+			),
 		)
 	})
 }
